d9df2032547032b6025dbe7b84236ccf8072a90a
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-03-23 21:25:00 -0700

    Working with express.

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 'use strict';
 
-//var express = require('express');
+var express = require('express');
+var app = express();
 
-//var app = express();
-
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Node\n");
+app.get('/', function(req, res){
+    res.send('Hello Node');
 });
+// Configure our HTTP server to respond with Hello World to all requests.
 
 // Start the app by listening on <port>
 var port = 8000;
-server.listen(port);
-console.log('Express app started on port ' + port);
+app.listen(port, function(){
+    console.log('Express app started on port ' + port);
+});
+
