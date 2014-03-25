cd6957aeffa4ecc2e32c6d62b9b357e82f2f43d0
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-03-24 20:35:42 -0700

    Index view is correctly being displayed.

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var express = require('express');
+var routes = require('./app/routes');
 var app = express();
+var path = require('path');
+var port = 8000;
 
-app.get('/', function(req, res){
-    res.send('Hello Node');
-});
-// Configure our HTTP server to respond with Hello World to all requests.
+app.set('port', port);
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
 
 // Start the app by listening on <port>
-var port = 8000;
 app.listen(port, function(){
     console.log('Express app started on port ' + port);
 });
